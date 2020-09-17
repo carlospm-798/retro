f17f70c362b0d899099c7cacee0c9a59a75df0b9
Carlos Paredes <carlos.paredes6900@alumnos.udg.mx>
2020-09-16 20:52:01 -0500

    Hablilite opción 2, mostrar n veces el arreglo, modifique la vista del programa y soluciones problema de los <if's> en la op. 2

--- retro.cpp
+++ retro.cpp
@@ -17,72 +17,102 @@ struct personaje{
 }proto[c];
 
 //función mostrar.
-//void show(int n, float arreglo[]);
+void show(int n, float arreglo[]);
 
 //función principal.
 int main(void){
 	
 	char res=0;
 	
+	printf("Actividad #4 seminario de algoritmia:\n\n\n");
+	
 	do{
 		int i;
 		int op;
-		int reg=0;
+		int n=0;
 		
 		float arreglo[5];
 		float suma=0;
 		float promedio=0;
 		
-	    printf("Actividad #4 seminario de algoritmia:\n\n\n");
-	    printf("Ingresar arreglo: [1]\nMostrar arreglo: [2]\nStruct de personajes: [3]\n\n\n");
-	    scanf("%i",&op); system("cls"); res=0;
-	    
+		char respuesta=0;
+		
 	    do{
-		    
-			switch(op){
 	    	
-		    	case 1:
+	        printf("Ingresar arreglo: [1]\nMostrar arreglo: [2]\nStruct de personajes: [3]\n\n\n");
+	        scanf("%i",&op); system("cls"); int reg;
+		    
+			    switch(op){
+	    	
+		    	    case 1:
 				
-		    		//recopilación de datos de arreglo.
-		    		printf("Ingresar arreglo de 5 digitos:\n\n\n");
-		    		    for(i=0;i<5;i++){
-		    		    	printf("Ingrese el valor del arreglo: [%i] -    ", i+1);
-		    			    scanf("%i",&arreglo[i]);
-		    		    }
+		    		    //recopilación de datos de arreglo.
+		    		    printf("Ingresar arreglo de 5 digitos:\n\n\n");
+		    		        for(i=0;i<5;i++){
+		    		        	printf("Ingrese el valor del arreglo: [%i] -    ", i+1);
+		    		    	    scanf("%f",&arreglo[i]);
+		    	    	    }
 				
-			    	//suma de areglo.
-			    	printf("\n\n\n");
-			        	for(i=0;i<5;i++){
-			    		suma = suma + arreglo[i];
-			    	}
-			    	printf("La suma del arreglo es: [%.f]\n",suma);
+			        	//suma de areglo.
+			        	printf("\n\n\n");
+			            	for(i=0;i<5;i++){
+			        		suma = suma + arreglo[i];
+			        	}
+			        	printf("La suma del arreglo es: [%.f]\n",suma);
 				
-			    	//promedio de arreglo.
-			    	promedio = suma/c;
-			    	printf("El promedio del arreglo: [%.f]\n",promedio);
-			    	reg++; //reg++ es para evitar mostrar el arreglo si no existe.
-			    	break;
+			        	//promedio de arreglo.
+			        	promedio = suma/c;
+			        	printf("El promedio del arreglo: [%.2f]\n\n\n",promedio);
+			        	reg = 1; //reg++ es para evitar mostrar el arreglo si no existe.
+			        	break;
 			
-		    	/*case 2:
-			    	if(reg==0){
-			    		printf("Aun no registra un arreglo.\n");
-			    	}
+	    	    	case 2:
+	    		    	if(reg==0){
+	    		    		printf("Aun no registra un arreglo.\n\n\n");
+	    		    		system("pause"); system("cls"); respuesta='s';
+	    		    	}
 				
-		    		if(reg!=0){
-		    			show(n,arreglo);
-		    		}
-			    	break;*/
+	    	    		if(reg==1){
+	    	    			printf("Cuantas veces quiere ver el arreglo?    ");
+	    	    			scanf("%i",&n); printf("\n\n\n");
+	    	    			show(n,arreglo);
+	    	    		}
+	    		    	break;
 			
-			default: printf("Error.\n");
-		}    	
-		}while(res=='s'||res=='S');
-	    
-
-	    
+	    		    default: printf("Error.\n");
+			
+			    printf("\n\n\nUsar otra funsion: [s] -    ");
+			    fflush(stdin); scanf("%c",&respuesta);
+			    
+		    }
+		    //fin switch.
+		}while(respuesta=='s'||respuesta=='S');
+	    system("pause"); system("cls");
 	    printf("Volver a repetir programa: [s]\n");
-	    fflush(stdin); scanf("%c",&res);
+	    fflush(stdin); scanf("%c",&res); system("cls");
 	    
 	}while(res=='s'||res=='S');
 	
 	return 0;
+}
+
+void show(int n, float arreglo[]){
+	
+	//mostrar en pantalla n veces el arreglo.
+	
+	int i;
+	int j;
+	
+	j=0;
+	while(j<n){
+		
+		for(i=0;i<5;i++){
+			//mostrar arreglo.
+			printf("%.f ",arreglo[i]);
+		}
+		printf("\n");
+		
+		j++;
+	}
+	printf("\n\n\n");
 }
